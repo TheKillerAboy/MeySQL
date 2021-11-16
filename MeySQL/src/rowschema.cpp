9979353d8d4c42adfb6b288f5c724921cf2b4b5a
Annekin Meyburgh <annekin.meyburgh@takealot.com>
2021-11-16 16:56:36 +0200

    added rowschema header and implemented into source

---
+++ MeySQL/src/rowschema.cpp
@@ -0,0 +1,26 @@
+#include "rowschema.h"
+
+MeySQL::RowSchema::RowSchema():MeySQL::RowSchema(0){
+}
+MeySQL::RowSchema::RowSchema(const size_t& size){
+    set_size(size);
+}
+
+MeySQL::RowSchema::~RowSchema(){
+}
+
+CellType MeySQL::RowSchema::get_index(const size_t& index) const{
+    return schema[index];
+}
+
+size_t MeySQL::RowSchema::get_size() const{
+    return schema.size;
+}
+
+void MeySQL::RowSchema::set_index(const size_t& index, const CellType& type){
+    schema[index] = type;
+}
+
+void MeySQL::RowSchema::set_size(const size_t& size){
+    schema.resize(size);
+}
