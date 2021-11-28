c962bb6fe70c3f4dd45534e7a442676bec66341d
Annekin Meyburgh <annekin.meyburgh@takealot.com>
2021-11-28 19:09:03 +0200

    Made system connect with python

--- include/connection.h
+++ include/connection.h
@@ -3,6 +3,7 @@
 
 #include "sockpp/tcp_socket.h"
 #include <thread>
+#include <string>
 
 namespace MeySQL{
     namespace Connect{
@@ -10,6 +11,8 @@ namespace MeySQL{
         private:
             sockpp::tcp_socket sock;
             std::thread thr;
+
+            static const size_t PAYLOAD_BUFFER_SIZE = 1024;
         public:
             Connection(sockpp::tcp_socket sock_);
             ~Connection();
@@ -17,6 +20,11 @@ namespace MeySQL{
             void start();
 
             static void thread_loop(Connection* con);
+
+            sockpp::tcp_socket& get_socket();
+
+            void send(const std::string& msg);
+            std::string recieve();
         };
     }
 }
--- src/connection.cpp
+++ src/connection.cpp
@@ -1,6 +1,8 @@
 #include "connection.h"
 #include <thread>
 #include "sockpp/tcp_socket.h"
+#include <string>
+#include <iostream>
 
 MeySQL::Connect::Connection::Connection(sockpp::tcp_socket sock_){
     sock = std::move(sock_);
@@ -8,11 +10,31 @@ MeySQL::Connect::Connection::Connection(sockpp::tcp_socket sock_){
 
 MeySQL::Connect::Connection::~Connection(){}
 
-void MeySQL::Connect::Connection::thread_loop(Connection* con){
 
+
+void MeySQL::Connect::Connection::thread_loop(Connection* con){
+    std::string msg = con->recieve();
+    con->send("Hello World");
 }
 
 void MeySQL::Connect::Connection::start(){
     thr = std::thread(MeySQL::Connect::Connection::thread_loop, this);
     thr.detach();
+}
+
+sockpp::tcp_socket& MeySQL::Connect::Connection::get_socket(){
+    return sock;
+}
+
+void MeySQL::Connect::Connection::send(const std::string& msg){
+    std::cout << "Sending: " << msg << " ";
+    std::cout << get_socket().write(msg) << std::endl;
+}
+
+std::string MeySQL::Connect::Connection::recieve(){
+    char buf[PAYLOAD_BUFFER_SIZE];
+    size_t size =  get_socket().read(buf, PAYLOAD_BUFFER_SIZE);
+    std::string msg(buf, size);
+    std::cout << "Recieved: " << msg << std::endl;
+    return msg;
 }
