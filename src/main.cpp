6673175432a9eceeffbcac3b150c960fe07c4305
Annekin Meyburgh <annekin.meyburgh@takealot.com>
2021-11-28 23:42:23 +0200

    Added boost and test logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include "server.h"
+#include <iostream>
+#include <boost/log/trivial.hpp>
 
-int main(){
-    MeySQL::Connect::Server server;
-    server.run();
+int main(int, char*[])
+{
+    BOOST_LOG_TRIVIAL(error) << "This is an informational severity message";
+    return 0;
 }
-
